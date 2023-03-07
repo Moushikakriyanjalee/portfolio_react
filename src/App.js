96dfb44d1539dea1e216ac8fc77e096f721d87a1
moushikakri <123530282+moushikakri@users.noreply.github.com>
2023-03-07 23:24:11 +0530

    Update App.js
    after aos

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect} from "react";
 import Navbar from './common/Navbar';
 import Homepage from './screen/Homepage';
 import About from './screen/About';
@@ -8,8 +8,15 @@ import Skills from './screen/Skills';
 import Contact from './screen/Contact';
 import Footer from './screen/Footer';
 import './App.css';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
 function App() {
+  
+  useEffect(() => {
+    AOS.init();
+  }, []);
+  
   return (
     <>
     <Navbar/>
